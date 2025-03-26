128b1a4108896a5c5ce741a312b5776b4f61002b
isabelbella141 <iscrisgo1590@gmail.com>
2025-03-25 20:50:24 -0500

    acrualizar

--- backend/server copy.js
+++ backend/server copy.js
@@ -20,7 +20,7 @@ app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 */
-require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -29,15 +29,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-//app.use(cors());
-// Configuración de CORS más específica
-app.use(cors({
-  origin: [
-    'http://localhost:3000', 
-    'https://tu-dominio-vercel.vercel.app'
-  ]
-}));
-
+app.use(cors());
 app.use(bodyParser.json());
 
 // Datos de usuarios de ejemplo (en un escenario real, usarías una base de datos)
@@ -66,15 +58,6 @@ app.post('/api/login', (req, res) => {
 });
 
 // Iniciar servidor
-/*
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
-});*/
-
-// Iniciar servidor
-const server = app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
-
-// Exportar para testing o serverless
-module.exports = { app, server };
